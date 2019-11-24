95961e79c493c67fde0f6d9a14b2cf7bdf971548
kyplyk <avdosev1@yandex.ru>
2019-11-24 22:36:56 +0400

    вывод букв на дисплей

--- lcd.py
+++ lcd.py
@@ -5,73 +5,87 @@ import time
 
 # pins = [21,26,20]
 # lcdPins = [16,19,13,12]
-namedPins = { # BCM
+namedPins = {  # BCM
     'RS': 21,
     'RW': 26,
     'EN': 20,
 }
 
 digitalPins = [
-    16,19,13,12
-] 
+    16, 19, 13, 12
+]
+
 
 def set_on_pin(pin, power):
     gpio.output(pin, gpio.HIGH if power else gpio.LOW)
 
+
 def set_low(pin):
     gpio.output(pin, gpio.LOW)
+
 
 def set_high(pin):
     gpio.output(pin, gpio.HIGH)
 
+
 def set_line(bits):
-    for i in range(4):
+    for i in range(len(digitalPins)):
         set_on_pin(digitalPins[i], bits[i])
 
+
 def send(byte):
+    bits = byte_to_bits(byte)
     time_to_sleep = 0.1
-    set_low(namedPins["RS"])
     set_low(namedPins["EN"])
-    set_line([0,0,1,0])
+    set_line(bits[0:4])
     set_high(namedPins["EN"])
     time.sleep(time_to_sleep)
-    set_low(namedPins["EN"]) #6
-    time.sleep(time_to_sleep)
-    set_line([1,0,0,0])
+    set_low(namedPins["EN"])
+    set_line(bits[4:8])
     set_high(namedPins["EN"])
     time.sleep(time_to_sleep)
-    set_low(namedPins["EN"]) 
-    time.sleep(time_to_sleep)
+    set_low(namedPins["EN"])
+
+
+def send_command(command):
+    set_high(namedPins["RS"])
+    send(command)
+    set_low(namedPins["RS"])
+
+
+def byte_to_bits(byte):
+    bits = []
+    for i in range(8):
+        bits.append(byte & 1)
+        byte >>= 1
+    bits.reverse()
+    return bits
 
 
 def clear():
     send(0b00000001)
 
-def cursorToHome():
+
+def cursor_to_home():
     send(0b00000010)
 
-def cursorToHome():
-    send(0b00000010)
 
 def init():
-    gpio.setmode(gpio.BCM)                 
-    
+    gpio.setmode(gpio.BCM)
+
     for pin in namedPins.values():
         gpio.setup(pin, gpio.OUT)
 
     for pin in digitalPins:
         gpio.setup(pin, gpio.OUT)
 
+    send_command(0b00101000)
 
 
 def show_char(char):
-    pass
+    send(ord(char))
 
 
-def foo(halfByte): # 4 цифры
-    gpio.output(pins['RS'], gpio.LOW)
-    gpio.output(pins['EN'], gpio.LOW)
-
-    for pin in lcdPins:
-        gpio.output(pin, gpio.HIGH)
-
+def show_line(string):
+    for char in string:
+        show_char(char)
